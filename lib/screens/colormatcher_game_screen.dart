73b8393ff77f676c970eab1d8a5172c8c6917e4b
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-02-07 00:28:24 -0700

    Finishing match color game

--- lib/screens/colormatcher_game_screen.dart
+++ lib/screens/colormatcher_game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app_juegos/constants/change_colors.dart';
+import 'package:app_juegos/widgets/game_over_dialog.dart';
 import 'package:flutter/material.dart';
 
 class ColorMatcherGame extends StatefulWidget {
@@ -8,24 +9,50 @@ class ColorMatcherGame extends StatefulWidget {
   State<ColorMatcherGame> createState() => _ColorMatcherGameState();
 }
 
-class _ColorMatcherGameState extends State<ColorMatcherGame> {
+class _ColorMatcherGameState extends State<ColorMatcherGame>
+    with SingleTickerProviderStateMixin {
   MapEntry<Color, String> _backgroundColor = getSelectedColor();
   MapEntry<Color, String> _colorName = getSelectedColor();
   int _score = 0;
   bool _doColorsMatch = false;
+  double _timeLeft = 3;
+  late AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
     _doColorsMatch = doColorMatch();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..addListener(() {
+        setState(() {
+          _timeLeft =
+              _controller.duration!.inSeconds * (1.0 - _controller.value);
+          if (_timeLeft <= 0) {
+            gameOver();
+          }
+        });
+      });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void restartTimer() {
+    _timeLeft = 3;
+    _controller.reset();
+    _controller.forward();
   }
 
   void generateAnotherColor() {
-    setState(() {
-      _backgroundColor = getSelectedColor();
-      _colorName = getSelectedColor();
-      _doColorsMatch = doColorMatch();
-    });
+    _backgroundColor = getSelectedColor();
+    _colorName = getSelectedColor();
+    _doColorsMatch = doColorMatch();
   }
 
   bool doColorMatch() {
@@ -35,34 +62,19 @@ class _ColorMatcherGameState extends State<ColorMatcherGame> {
   void resetGame() {
     setState(() {
       _score = 0;
-      _backgroundColor = getSelectedColor();
-      _colorName = getSelectedColor();
-      _doColorsMatch = doColorMatch();
+      generateAnotherColor();
     });
-  }
-
-  void updateScore() {
-    setState(() {
-      _score++;
-    });
+    restartTimer();
   }
 
   void gameOver() {
+    _controller.reset();
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Game Over'),
-          content: Text('Your score is $_score'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                resetGame();
-              },
-              child: const Text('Play Again'),
-            ),
-          ],
+        return GameOverDialog(
+          points: _score,
+          startGame: resetGame,
         );
       },
     );
@@ -72,41 +84,59 @@ class _ColorMatcherGameState extends State<ColorMatcherGame> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Color Matcher'),
+        title: Text('$_score'),
       ),
       body: Container(
         color: _backgroundColor.key,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
+            LinearProgressIndicator(
+              value: _timeLeft / 3,
+              color: Colors.green,
+            ),
             Center(
               child: Text(
-                "${_colorName.value}  $_doColorsMatch $_score",
+                _colorName.value,
                 style: const TextStyle(fontSize: 50),
               ),
             ),
-            IconButton(
-              color: const Color.fromARGB(255, 10, 108, 13),
-              iconSize: 50,
-              onPressed: () {
-                if(_doColorsMatch == true){
-                  generateAnotherColor();
-                  _score++;
-                }
-              },
-              icon: const Icon(Icons.check),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  color: const Color.fromARGB(255, 10, 108, 13),
+                  iconSize: 50,
+                  onPressed: () {
+                    if (_doColorsMatch == true) {
+                      generateAnotherColor();
+                      _score++;
+                      restartTimer();
+                    } else {
+                      gameOver();
+                    }
+                  },
+                  icon: const Icon(Icons.check),
+                ),
+                IconButton(
+                  color: const Color.fromARGB(255, 163, 18, 8),
+                  iconSize: 50,
+                  onPressed: () {
+                    if (_doColorsMatch == false) {
+                      generateAnotherColor();
+                      _score++;
+                      restartTimer();
+                    } else {
+                      gameOver();
+                    }
+                  },
+                  icon: const Icon(Icons.cancel),
+                ),
+              ],
             ),
-            IconButton(
-              color: const Color.fromARGB(255, 163, 18, 8),
-              iconSize: 50,
-              onPressed: () {
-                if(_doColorsMatch == false){
-                  generateAnotherColor();
-                  _score++;
-                }
-              },
-              icon: const Icon(Icons.cancel),
-            ),
+            const SizedBox(
+              height: 5,
+            )
           ],
         ),
       ),
